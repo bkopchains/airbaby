cacdd2a6191d7be423ad90eb573b90714b14c9be
Ben Kopchains <bkopchains@dynamis.com>
2024-03-16 08:24:59 -0400

    landing page hero

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,20 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import { Hero } from "@/components/hero/hero";
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      <Image
-        className={styles.logo}
-        src="/airbaby_text.svg"
-        alt="airbaby logo, baby"
-        width={400}
-        height={400}
-        priority
-      />
+      <div>
+        <Image
+          className={styles.logo}
+          src="/airbaby_text.svg"
+          alt="airbaby logo, baby"
+          width={350}
+          height={150}
+        />
+      </div>
+      <Hero />
     </main>
   );
 }
---
+++ src/components/hero/hero.tsx
@@ -0,0 +1,47 @@
+import Image from 'next/image';
+import { Container, Text, Button, Group, MantineGradient } from '@mantine/core';
+import classes from './hero.module.css';
+
+const gradient: MantineGradient = { from: 'pink', to: 'blue' }
+
+export function Hero() {
+  return (
+    <div className={classes.wrapper}>
+      <Container size={700} className={classes.inner}>
+        <h1 className={classes.title}>
+          Building{' '}
+          <Text component="span" variant="gradient" gradient={gradient} inherit>
+            totally safe
+          </Text>{' '}
+          solutions for you and your babies
+        </h1>
+
+        <Text className={classes.description}>
+          From the brilliant mind behind RentABaby™️, comes exciting new business solutions! Stay tuned for more.
+        </Text>
+
+        <Group className={classes.controls}>
+          <Button
+            size="xl"
+            className={classes.control}
+            variant="gradient"
+            gradient={gradient}
+          >
+            Get started
+          </Button>
+
+          <Button
+            component="a"
+            href="https://github.com/bkopchains/airbaby"
+            size="xl"
+            variant="default"
+            className={classes.control}
+            leftSection={<Image className={classes.logo} src="/airbaby.svg" alt="airbaby" width={20} height={20} />}
+          >
+            GitHub
+          </Button>
+        </Group>
+      </Container>
+    </div>
+  );
+}

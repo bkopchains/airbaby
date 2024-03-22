d9459979cc7421abb5fb7a113a9b18f0113fa9cb
Ben Kopchains <bkopchains@dynamis.com>
2024-03-22 14:06:20 -0400

    clickable icons

--- src/components/mobileDrawer/mockLinks.tsx
+++ src/components/mobileDrawer/mockLinks.tsx
@@ -1,43 +1,62 @@
-import { UnstyledButton, Group, ThemeIcon, rem, Text, useMantineTheme } from "@mantine/core";
-import { IconBarbell, IconHeartHandshake, IconHearts } from "@tabler/icons-react";
-import styles from "./mobileDrawer.module.css"
+import {
+  UnstyledButton,
+  Group,
+  ThemeIcon,
+  rem,
+  Text,
+  useMantineTheme,
+} from "@mantine/core";
+import {
+  IconBarbell,
+  IconHeartHandshake,
+  IconHearts,
+} from "@tabler/icons-react";
+import styles from "./mobileDrawer.module.css";
 import Link from "next/link";
 
-const mockdata: { icon: any, title: string, description: string, href: string }[] = [
+const mockdata: {
+  icon: any;
+  title: string;
+  description: string;
+  href: string;
+}[] = [
   {
     icon: IconHeartHandshake,
-    title: 'RentABaby',
-    description: 'rent a baby, baby',
-    href: '/rentababy'
+    title: "RentABaby",
+    description: "rent a baby, baby",
+    href: "/rentababy",
   },
   {
     icon: IconHearts,
-    title: 'BabyMatch',
-    description: 'babies deserve love too, baby',
-    href: '/babymatch'
+    title: "BabyMatch",
+    description: "babies deserve love too, baby",
+    href: "/babymatch",
   },
   {
     icon: IconBarbell,
-    title: 'Buff Baby',
-    description: 'get swole, baby',
-    href: '/buffbaby'
-  }
+    title: "Buff Baby",
+    description: "get swole, baby",
+    href: "/buffbaby",
+  },
 ];
 
 interface IMockLinks {
-  close: () => void
+  close: () => void;
 }
 
 export const MockLinks = (props: IMockLinks) => {
-  const {close} = props;
+  const { close } = props;
   const theme = useMantineTheme();
   return mockdata.map((item) => (
     <UnstyledButton className={styles.subLink} key={item.title}>
-      <Group wrap="nowrap" align="flex-start">
-        <ThemeIcon size={34} variant="default" radius="md">
-          <item.icon style={{ width: rem(22), height: rem(22) }} color={theme.colors.pink[6]} />
-        </ThemeIcon>
-        <Link onClick={close} href={item.href}>
+      <Link onClick={close} href={item.href}>
+        <Group wrap="nowrap" align="flex-start">
+          <ThemeIcon size={34} variant="default" radius="md">
+            <item.icon
+              style={{ width: rem(22), height: rem(22) }}
+              color={theme.colors.pink[6]}
+            />
+          </ThemeIcon>
           <div>
             <Text size="md" fw={500}>
               {item.title}
@@ -46,8 +65,8 @@ export const MockLinks = (props: IMockLinks) => {
               {item.description}
             </Text>
           </div>
-        </Link>
-      </Group>
+        </Group>
+      </Link>
     </UnstyledButton>
-  ))
+  ));
 };
